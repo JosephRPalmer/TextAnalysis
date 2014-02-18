c5eeff2fc5850205db67c45febdcdb683abd9a07
Joseph Palmer <joseph@ryan-palmer.com>
2014-02-18 13:23:35 +0000

    Create Front End of Applet

--- src/TextAnalysis.java
+++ src/TextAnalysis.java
@@ -1,5 +1,36 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class TextAnalysis {
+public class TextAnalysis extends Applet implements ActionListener {
+	
+	Button a,b;
+	TextField tf;
+	
+	public void init (){
+		
+		setSize(500, 500);
+		
+		tf = new TextField(35);
+		add(tf);
+		tf.addActionListener(this);
+		
+		a = new Button("Analyze");
+		add(a);
+		a.addActionListener(this);
+		
+		b = new Button("Reset");
+		add(b);
+		b.addActionListener(this);
+		
+		
+	}
+	
+	public void actionPerformed (ActionEvent e){
+		
+	}
+	
 
 	
 
