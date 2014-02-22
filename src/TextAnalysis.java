1d78506e025f043cc2ff87e5ba958f136a84c84d
Joseph Palmer <joseph@ryan-palmer.com>
2014-02-22 21:32:56 +0000

    PASS
    Pass level code

--- src/TextAnalysis.java
+++ src/TextAnalysis.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 @SuppressWarnings({ "serial", "unused" })
 public class TextAnalysis extends Applet implements ActionListener {
-	
+	boolean thereistext;
 	Button a,b;
 	TextField tf;
 	String input;
-	int [] len;
+	
 	
 	//line 65
 	public void init (){
@@ -34,11 +34,48 @@ public class TextAnalysis extends Applet implements ActionListener {
 	
 	public void start() {
 		tf.setText("Enter Text Here...");
-		
+		thereistext = false;
 	}
 	
 	public void paint (Graphics g){
-		
+		if (thereistext){
+			
+			
+			input = tf.getText();
+			String [] parts = input.split(" ");
+			int templen = 0;
+			
+			for (int i = 0; i <= parts.length-1; i++){
+				
+				if (templen < parts[i].length()){
+					
+					templen = parts[i].length();
+				}
+				
+			}
+			
+			System.out.println(templen);
+			
+			int len[] = new int[templen+1];
+				
+			for (int i1 = 0; i1 <= parts.length-1;i1++){
+				
+				
+				
+				len[parts[i1].length()]++;
+				
+			}
+			int printblank = 100;
+			for (int i2 = 0; i2 != len.length;i2++){
+				
+				if (len[i2] != 0){
+					g.drawString("There are " + len[i2] + " words that are " + i2 + " long", 100,printblank );
+					printblank +=20;
+				}
+				
+			}
+			
+			}
 	}
 	
 	
@@ -46,7 +83,8 @@ public class TextAnalysis extends Applet implements ActionListener {
 	public void actionPerformed (ActionEvent e){
 		
 		if (e.getSource()==a){
-			Parting(null);
+			thereistext=true;
+			repaint();
 		}
 		if (e.getSource()==b){
 			start();
@@ -54,28 +92,6 @@ public class TextAnalysis extends Applet implements ActionListener {
 		
 	}
 	
-	public void Parting (Graphics g){
-		input = tf.getText();
-		String [] parts = input.split(" ");
-			
-		for (int i1 = 0; i1 != parts.length;i1++){
-			
-			int templen;
-			templen = parts[i1].length();
-			len[templen] +=1;
-			
-		}
-		
-		for (int i2 = 0; i2 != len.length;i2++){
-			
-			if (len[i2] != 0){
-				g.drawString("There are " + len[i2] + " words that are " + i2 + " long", 100,100 );
-			}
-			
-		}
-		
-		
-	}
 	
 
 	
